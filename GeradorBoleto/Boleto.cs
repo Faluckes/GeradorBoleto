35f259d25b6106845691b78920c7b96d7ea8cbba
Faluckes <Faluckes@DESKTOP-EHHI3AM>
2022-03-03 21:20:04 -0300

    Adicionar arquivos de projeto.

---
+++ GeradorBoleto/Boleto.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GeradorBoleto
+{
+    public class Boleto
+    {
+        public string Data { get; set; }
+
+        public double ValorBoleto { get; set; }
+
+        public string NumeroBanco { get; set; }
+
+        public string NumeroBoleto { get; set; }
+
+        public string NumeroGrande = "00000000";
+
+        private string _valortotal;
+
+        public Boleto(double valorboleto, string numerobanco)
+        {
+            ValorBoleto = valorboleto;
+            NumeroBanco = numerobanco;
+            Data = DateTime.Now.ToString("dd/MM/yyyy");
+
+
+            Random numBoleto = new Random();
+            string tempNum = numBoleto.Next(1000, 10000).ToString();
+
+            while (tempNum.Length < 5)
+            {
+                tempNum = string.Concat("0", tempNum);
+            }
+            NumeroBoleto = tempNum;
+        }
+        public string ValorTotal
+        {
+            get
+            {
+                return string.Concat(NumeroBanco + " | " + NumeroBoleto + DateTime.Now.ToString("ddMMyyyy") + NumeroGrande + ValorBoleto);
+            }
+            set
+            {
+                _valortotal = value;
+            }
+        }
+
+
+    }
+
+}
---
+++ GeradorBoleto/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace GeradorBoleto
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        Boleto boleto = new Boleto(200, "334-3");
+
+            Console.WriteLine("Data do Boleto: " + boleto.Data);
+
+            Console.WriteLine("Banco: " + boleto.NumeroBanco + "\n" + "Valor: R$" + boleto.ValorBoleto);
+
+            Console.WriteLine("Número do Boleto: " + boleto.NumeroBoleto);
+
+            Console.WriteLine(boleto.ValorTotal);
+
+            Console.ReadLine();
+        }
+    }
+}
