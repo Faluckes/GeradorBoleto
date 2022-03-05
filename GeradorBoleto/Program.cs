06f427a929ed97ee3140fda7e4a7178cd90d9aa6
Faluckes <FaluckesDev@protonmail.com>
2022-03-04 21:34:28 -0300

    Linhas de Código reformuladas.

--- GeradorBoleto/Program.cs
+++ GeradorBoleto/Program.cs
@@ -6,23 +6,32 @@ namespace GeradorBoleto
     {
         static void Main(string[] args)
         {
+            // Coloque seu numero do banco.
             Console.Write("Coloque o Numero do Banco: ");
             string numerodobanco = Console.ReadLine();
             Console.WriteLine("Usuario do Banco: " + numerodobanco + "\n");
 
-            Console.Write("Coloque o Valor do Boleto: ");
-            string valordoboleto = Console.ReadLine().Replace(".", ",");
             double valorboletocerto = default;
-            if (double.TryParse(valordoboleto, out double resultado))
-            {
-                valorboletocerto = resultado;
-                Console.WriteLine("O Valor do Boleto é de R$" + valordoboleto + "\n");
-            }
-            else
-            {
-                Console.WriteLine("Valor invalido");
-            }
+            bool iConventer = false;
 
+            // Coloque um Valor Válido para gerar o seu boleto!
+            while (!iConventer)
+            {
+                Console.Write("Coloque o Valor do Boleto: ");
+                string valordoboleto = Console.ReadLine().Replace(".", ",");
+
+                if (double.TryParse(valordoboleto, out double resultado))
+                {
+
+                    Console.WriteLine($"O Valor do Boleto é de R$ {valordoboleto}" + "\n");
+                    valorboletocerto = resultado;
+                    iConventer = true;
+                }
+                else
+                {
+                    Console.WriteLine("Valor invalido, por favor coloque um valor valido!");
+                }
+            }
             Boleto boleto = new Boleto(numerodobanco, valorboletocerto);
 
 
